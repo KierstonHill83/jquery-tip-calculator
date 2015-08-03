63befcf8a93c527c5d68057d5651dd171d53e8f2
KierstonHill83 <khill1218@gmail.com>
2015-08-03 08:45:35 -0600

    Added javascript for the twenty percent tip

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-console.log("Hello");
+function tipTwenty(num) {
+  return (num * 0.2).toFixed(2);
+}
 
+
+
+document.getElementsByClassName("btn")[0].addEventListener("click", function() {
+    var amount = document.getElementsByTagName("input")[0].value;
+    var twentyPercentTip = tipTwenty(amount);
+    var tipAmount = document.getElementsByTagName("h2")[0];
+    tipAmount.innerHTML = "You should tip $" + twentyPercentTip;
+  });
+
+
+//find box and selectedIndex
