d2a37dcd4d2c1cc0676ee8bbb07c62f6c902c4da
KierstonHill83 <khill1218@gmail.com>
2015-08-03 10:58:58 -0600

    Updated the calculate button so it will work when decimals and the dollar sign is inputed

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-function tipTwenty(num) {
+function tipTwentyPercent(num) {
   return (num * 0.2).toFixed(2);
 }
 
 
 
+
+//how to work when user puts in $
+
 document.getElementsByClassName("btn")[0].addEventListener("click", function() {
-    var amount = document.getElementsByTagName("input")[0].value;
-    var twentyPercentTip = tipTwenty(amount);
+    var amount = document.getElementsByTagName("input")[0].value.replace(/\$/g, "");
+    var twentyPercentTip = tipTwentyPercent(amount);
     var tipAmount = document.getElementsByTagName("h2")[0];
     tipAmount.innerHTML = "You should tip $" + twentyPercentTip;
   });
